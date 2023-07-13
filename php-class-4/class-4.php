bbbbf03107cee74a7d8c40e870d50382c89df87c
Md. Mahafuzur Rahman <mahafuzurrahmanhridoy@gmail.com>
2023-07-13 23:24:27 +0600

    class-4 v1

--- php-class-4/class-4.php
+++ php-class-4/class-4.php
@@ -1,12 +1,3 @@
 <?php
 
-$department = [
-    'cse' => array('Seam', 'Erfan'),
-    'bba' => ['Ananna', 'Mehedi']
-];
-
-foreach($department as $department){
-    
-}
-
-echo $department [0][1];
+echo "Hello";
