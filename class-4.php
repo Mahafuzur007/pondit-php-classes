aaf91105cc9fb3b3b45a887272fabcefbddeafb8
Md. Mahafuzur Rahman <mahafuzurrahmanhridoy@gmail.com>
2023-07-15 13:40:32 +0600

    Class- 4 HomeWork

--- class-4.php
+++ class-4.php
@@ -8,144 +8,144 @@
 <body>
 <?php
 
-// // index_array:
-// $students = [
-//     'messi',
-//     'dimaria',
-//     'dibu'
-// ];
-// echo "<pre>";
-// print_r($students);
-// var_dump($students);
+// index_array:
+$students = [
+    'messi',
+    'dimaria',
+    'dibu'
+];
+echo "<pre>";
+print_r($students);
+var_dump($students);
 
-// // associative_array:
-// $students = [
-//     'stud1' => 'messi',
-//     'stud2' => 'dimaria',
-//     'stud3' => 'dibu'
-// ];
-// echo "<pre>";
-// print_r($students);
-// var_dump($students);
-// echo $students ['stud3'];
+// associative_array:
+$students = [
+    'stud1' => 'messi',
+    'stud2' => 'dimaria',
+    'stud3' => 'dibu'
+];
+echo "<pre>";
+print_r($students);
+var_dump($students);
+echo $students ['stud3'];
 
 // for_loop:
-// $students = [
-//     '0' => 'messi',
-//     '1' => 'dimaria',
-//     '2' => 'dibu'
-// ];
-// echo "<pre>";
-// print_r($students);
-// for($i=0; $i<3 ; $i++){
-//     echo $students [$i] ."<pre>";
-// }
+$students = [
+    '0' => 'messi',
+    '1' => 'dimaria',
+    '2' => 'dibu'
+];
+echo "<pre>";
+print_r($students);
+for($i=0; $i<3 ; $i++){
+    echo $students [$i] ."<pre>";
+}
 
-// // count_function:
-// $students = [
-//     '0' => 'messi',
-//     '1' => 'dimaria',
-//     '2' => 'dibu'
-// ];
+// count_function:
+$students = [
+    '0' => 'messi',
+    '1' => 'dimaria',
+    '2' => 'dibu'
+];
 
-// for($i=0; $i<count($students) ; $i++){
-//     echo $students [$i] ."<pre>";
-// }
+for($i=0; $i<count($students) ; $i++){
+    echo $students [$i] ."<pre>";
+}
 
-// for($i=0, $elements = count($students); $i<$elements ; $i++){
-//     echo $students [$i] ."<pre>";
-// }
+for($i=0, $elements = count($students); $i<$elements ; $i++){
+    echo $students [$i] ."<pre>";
+}
 
-// // foreach_loop:
-// $students = [
-//     'stu0' => 'messi',
-//     'stu1' => 'dimaria',
-//     'stu2' => 'dibu'
-// ];
+// foreach_loop:
+$students = [
+    'stu0' => 'messi',
+    'stu1' => 'dimaria',
+    'stu2' => 'dibu'
+];
 
-// $students = [
-//     '0' => 'messi',
-//     '1' => 'dimaria',
-//     '2' => 'dibu'
-// ];
-// foreach($students as $index=>$name){
-//     echo $index.'- '.$name."<pre>";
-// }
+$students = [
+    '0' => 'messi',
+    '1' => 'dimaria',
+    '2' => 'dibu'
+];
+foreach($students as $index=>$name){
+    echo $index.'- '.$name."<pre>";
+}
 
 
-// // multidimensional_array/multiple array:
-// $departments = [
-//     'bba' => ['ronaldo','mbappe'],
-//     'cse' => ['messi' , 'benzema']
-// ];
+// multidimensional_array/multiple array:
+$departments = [
+    'bba' => ['ronaldo','mbappe'],
+    'cse' => ['messi' , 'benzema']
+];
 
-// echo"<pre>";
-// print_r($departments);
+echo"<pre>";
+print_r($departments);
 
-// foreach($departments as $departmentKey => $departmentValue){
-//     echo $departmentKey;
-//     echo strtoupper($departmentKey)."<br>"; // ["strtoupper for Uppercase function"]
-//     foreach($departmentValue as $index => $value){
-//         echo (($index+1)."-".$value)."<br>";
-//     }
-// }
+foreach($departments as $departmentKey => $departmentValue){
+    echo $departmentKey;
+    echo strtoupper($departmentKey)."<br>"; // ["strtoupper for Uppercase function"]
+    foreach($departmentValue as $index => $value){
+        echo (($index+1)."-".$value)."<br>";
+    }
+}
 
-// // Constraints Variable:
-// define('student_name','Messi'); // define(Name,value)
-// echo student_name;
+// Constraints Variable:
+define('student_name','Messi'); // define(Name,value)
+echo student_name;
 
 define ('num' , 500);
 echo num;
 
-// // Tarnary Operator:
+// Tarnary Operator:
 
-// $x=5;
-// if($x == 5){
-//     echo 'Five';
-// }else{
-//     echo 'Others';
-// }
-// echo $x==5 ? 'five' : 'others'; // Tarnary Operator:
+$x=5;
+if($x == 5){
+    echo 'Five';
+}else{
+    echo 'Others';
+}
+echo $x==5 ? 'five' : 'others'; // Tarnary Operator:
 
 
-// // isset & unset:
-// $x = 5;
-// $y = null;
-// unset ($y);
+// isset & unset:
+$x = 5;
+$y = null;
+unset ($y);
 
-// if(isset($y)){
-//     echo 'Variable found';
-// }else{
-//     echo 'Variable is not found';
-// }
+if(isset($y)){
+    echo 'Variable found';
+}else{
+    echo 'Variable is not found';
+}
 
-// // Null Coalescing Operator:
-// echo $y ?? "Variable is not set";
+// Null Coalescing Operator:
+echo $y ?? "Variable is not set";
 
-// // function:
+// function:
 
-// function alarm(){
-//     echo 'Good Morning';
-// }
-// alarm();
+function alarm(){
+    echo 'Good Morning';
+}
+alarm();
 
-// function alarm2($name){
-//     return 'Good Morning '.$name;
-// }
-// echo alarm('Bangladesh'); //uses of aurguments.
+function alarm2($name){
+    return 'Good Morning '.$name;
+}
+echo alarm('Bangladesh'); //uses of aurguments.
 
-// function sum($num1, int $num2){
-//     return $num1+$num2;
-// }
-// echo sum(1,'20b10b');
+function sum($num1, int $num2){
+    return $num1+$num2;
+}
+echo sum(1,'20b10b');
 
-// String Function:
+String Function:
 
 $name = 'Mahafuzur Rahman';
 echo ($name).'<br>';
 
 // strlen = string length function
-// echo 'The length of my string is ' . strlen ($name);
+echo 'The length of my string is ' . strlen ($name);
 
 // str_word_count = counting words in string
 echo str_word_count ($name);
