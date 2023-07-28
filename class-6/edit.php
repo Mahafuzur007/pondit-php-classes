6078a47359bbf10fb4fc388fe877f8c156d8203b
Md. Mahafuzur Rahman <mahafuzurrahmanhridoy@gmail.com>
2023-07-28 21:27:26 +0600

    Create a student database using session

--- class-6/edit.php
+++ class-6/edit.php
@@ -5,53 +5,41 @@ $index = $_GET['index'];
 
 $student = $_SESSION['students'][$index];
 
-
-if (isset($student)){
-  $id = $student['id'];
-  $name = $student['name'];
-  $class = $student['class'];
-  $number = $student['number'];
-
-} else {
-
-  echo "No data found!!!";
- 
-};
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <title>Edit</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <title>Edit</title>
 </head>
+
 <body>
-<div class="container my-5">
-<form action="update.php" method="POST">
-  <input type="hidden" name="index" value = <?= $index ?>>
-  <div class="mb-3">
-      <label class="form-label">Student Name</label>
-      <input type="text" class="form-control" placeholder="Enter Student Name" name="name" value = <?= $name ?> >
-    </div>
-    
-    <div class="mb-3">
-      <label class="form-label">Enter ID</label>
-      <input type="text" class="form-control" placeholder="Enter Student ID" name="id" value = <?= $id ?>>
-    </div>
-    
-    <div class="mb-3">
-      <label class="form-label">Class</label>
-      <input type="text" class="form-control" placeholder="" name="class" value = <?= $class ?>>
-    </div>
-    
-    <div class="mb-3">
-      <label class="form-label">Phone Number</label>
-      <input type="text" class="form-control" placeholder="" name="number" value = <?= $number ?>>
-    </div>
-    <button type="submit" class="btn btn-primary" name="submit">Update</button>
+  <div class="container my-5">
+    <form action="update.php?index=<?= $index ?>" method="POST">
+      <div class="mb-3">
+        <label class="form-label">Student Name</label>
+        <input type="text" class="form-control" placeholder="Enter Student Name" name="name" value=<?= $student['name'] ?>>
+      </div>
+
+      <div class="mb-3">
+        <label class="form-label">Enter ID</label>
+        <input type="text" class="form-control" placeholder="Enter Student ID" name="id" value=<?= $student['id'] ?>>
+      </div>
+
+      <div class="mb-3">
+        <label class="form-label">Class</label>
+        <input type="text" class="form-control" placeholder="" name="class" value=<?= $student['class'] ?>>
+      </div>
+
+      <div class="mb-3">
+        <label class="form-label">Phone Number</label>
+        <input type="text" class="form-control" placeholder="" name="number" value=<?= $student['number'] ?>>
+      </div>
+      <button type="submit" class="btn btn-primary" name="submit">Update</button>
     </form>
 </body>
-</html>    
 
-
+</html>
--- class-6/update.php
+++ class-6/update.php
@@ -1,27 +1,13 @@
 <?php
 session_start();
 
-$id = $_POST['id'];
-$name = $_POST['name'];
-$class = $_POST['class'];
-$number = $_POST['number'];
-$index = $_POST['index'];
+$index = $_GET['index'];
 
-foreach ($_SESSION['students'] as $key => $value)
-{
-    if ($key == $index)
-    {
-        $_SESSION['students'][$key]['id'] = $id;
-        $_SESSION['students'][$key]['name'] = $name;
-        $_SESSION['students'][$key]['class'] = $class;
-        $_SESSION['students'][$key]['number'] = $number;
-
-
-        header('location: ./table.php');
-    }
-}
+$_SESSION['students'][$index]['id'] = $_POST['id'];
+$_SESSION['students'][$index]['name'] = $_POST['name'];
+$_SESSION['students'][$index]['class'] = $_POST['class'];
+$_SESSION['students'][$index]['number'] = $_POST['number'];
 
 $_SESSION['message'] = 'Updated Sucessfully!';
 
-
-?>
+header('location: ./table.php');
